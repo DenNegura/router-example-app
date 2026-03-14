8166105e491f174fca557070df4b521ebfa30b18
Denis Negura <den.negura@gmail.com>
2026-03-14 16:32:09 +0200

    get post id from params (useParams)

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,27 @@
+import {useParams} from "react-router";
+import {useEffect, useState} from "react";
+
 function PostPage() {
-    return <h1>Post Page (for the selected post)</h1>
+    const {id} = useParams();
+
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/posts/" + id)
+            .then((res) => res.json())
+            .then(setPost)
+    })
+    return (
+        <div>
+            {post ?
+                <div>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </div> :
+                <h1>Loading...</h1>
+            }
+        </div>
+    )
 }
 
 export {PostPage}
