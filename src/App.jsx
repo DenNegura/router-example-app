f6cc8212a7c9fad02577f3c95f5356b2426e9748
Denis Negura <den.negura@gmail.com>
2026-03-14 14:20:44 +0200

    add dynamic route (PostPage), wildcard route (NotFoundPage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {Link, Route, Routes} from "react-router";
 import {HomePage} from "./pages/HomePage.jsx";
 import {PostsPage} from "./pages/PostsPage.jsx";
+import {NotFoundPage} from "./pages/NotFoundPage.jsx";
+import {PostPage} from "./pages/PostPage.jsx";
 
 function App() {
 
@@ -14,8 +16,10 @@ function App() {
             </header>
             <div className={"app-page-container"}>
                 <Routes>
-                    <Route path={"/"} element={<HomePage/>}/>
+                    <Route index element={<HomePage/>}/>
                     <Route path={"/posts"} element={<PostsPage/>}/>
+                    <Route path={"/posts/:id"} element={<PostPage/>}/>
+                    <Route path={"*"} element={<NotFoundPage/>}/>
                 </Routes>
             </div>
         </>
---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,5 @@
+function PostPage() {
+    return <h1>Post Page (for the selected post)</h1>
+}
+
+export {PostPage}
