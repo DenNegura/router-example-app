0a08532217a7ba9364cd0b2033c685cdb676a3ea
Denis Negura <den.negura@gmail.com>
2026-03-14 15:22:45 +0200

    use programming navigation (useNavigate)

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,22 @@
+import {useNavigate} from "react-router";
+
 function NotFoundPage() {
-    return <h1>404 - Page Not Found</h1>
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <div>
+                <p>The page you are looking for does not exist.</p>
+            </div>
+            <div className={"notfound-button-container"}>
+                <button onClick={() => navigate(-1)}>Go Back</button>
+                <button onClick={() => navigate("/")}>Go Home</button>
+                <button onClick={() => navigate("/posts")}>Go to Posts</button>
+            </div>
+        </div>
+    )
 }
 
 export {NotFoundPage};
